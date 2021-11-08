9dc1d1aaeebece61b753ec4c8d42e88d598110b1
Elad Ciuraru <eladciuraru@gmail.com>
2021-11-09 00:59:53 +0200

    Added loading for volume boot record data

--- ntfs_parser.h
+++ ntfs_parser.h
@@ -3,9 +3,22 @@
 #define _NTFS_PARSER_H
 
 #include <stdbool.h>
+#include <stdint.h>
+
+#define NTFS_Assert(Con, Msg) do { if (!(Con)) { __debugbreak(); } } while(0)
+
 
 typedef struct {
     void *Handle;
+
+    // Extracted
+    uint64_t SectorsPerCluster;
+    uint64_t MftCluster;
+    uint64_t BytesPerSector;
+
+    // Calcualted
+    uint64_t BytesPerCluster;
+    uint64_t BytesPerMftEntry;
 } ntfs_volume;
 
 ntfs_volume NTFS_VolumeOpen(char DriveLetter);
@@ -19,6 +32,41 @@ inline bool NTFS_VolumeIsValid(ntfs_volume *Volume)
     return Result;
 }
 
+
+#pragma pack(push, 1)
+
+typedef struct {
+    uint8_t  Jump[3];
+    uint8_t  OemID[8];
+    uint16_t BytesPerSector;
+    uint8_t  SectorsPerCluster;
+    uint16_t ReservedSectors;
+    uint8_t  Unsued0[3];
+    uint16_t Unused1;
+    uint8_t  MediaDescriptor;
+    uint16_t Unused2;
+    uint16_t SectorsPerTrack;
+    uint16_t NumberOfHeads;
+    uint32_t HiddenSectors;
+    uint32_t Unused3;
+    uint32_t Unused4;
+    uint64_t TotalSectors;
+    uint64_t MftCluster;
+    uint64_t MftMirrorCluster;
+    int8_t   ClustersPerFileRecord;
+    uint8_t  Unused5[3];
+    int8_t   ClustersPerIndexBuffer;
+    uint8_t  Unused6[3];
+    uint64_t SerialNumber;
+    uint32_t Checksum;
+    uint8_t  Code[426];
+    uint16_t Signature;
+} ntfs_vbr;
+
+#define NTFS_VBR_SIGNATURE 0xAA55
+
+#pragma pack(pop)
+
 #endif  // _NTFS_PARSER_H
 
 
@@ -26,6 +74,19 @@ inline bool NTFS_VolumeIsValid(ntfs_volume *Volume)
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+
+
+static bool Win32_ReadFrom_(void *Buffer, size_t Size, uint64_t From, HANDLE Handle)
+{
+    NTFS_Assert(Size == (uint32_t)Size, "Not supporting reading 64bit size");
+
+    LARGE_INTEGER From_ = { .QuadPart = From };
+    SetFilePointerEx(Handle, From_, 0, FILE_BEGIN);
+
+    DWORD BytesRead = 0;
+    BOOL  Result    = ReadFile(Handle, Buffer, Size, &BytesRead, NULL);
+    return Result && BytesRead == Size;
+}
 
 ntfs_volume NTFS_VolumeOpen(char DriveLetter)
 {
@@ -40,6 +101,24 @@ ntfs_volume NTFS_VolumeOpen(char DriveLetter)
         goto SKIP;
     }
     Volume.Handle = VolumeHandle;
+
+    ntfs_vbr BootSector;
+    Win32_ReadFrom_(&BootSector, sizeof(BootSector), 0, Volume.Handle);
+    if (BootSector.Signature != NTFS_VBR_SIGNATURE) {
+        NTFS_VolumeClose(&Volume);
+        goto SKIP;
+    }
+
+    // TODO: Validate valid values
+    Volume.BytesPerSector    = BootSector.BytesPerSector;
+    Volume.SectorsPerCluster = BootSector.SectorsPerCluster;
+    Volume.MftCluster        = BootSector.MftCluster;
+    Volume.BytesPerCluster   = Volume.BytesPerSector * Volume.SectorsPerCluster;
+    if (BootSector.ClustersPerFileRecord < 0) {
+        Volume.BytesPerMftEntry = 1 << (-BootSector.ClustersPerFileRecord);
+    } else {
+        Volume.BytesPerMftEntry = BootSector.ClustersPerFileRecord * Volume.BytesPerCluster;
+    }
 
 SKIP:
     return Volume;
